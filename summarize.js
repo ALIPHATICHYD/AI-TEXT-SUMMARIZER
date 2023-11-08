c100434a0c886327d0cfa92dd304b24f39a27ab8
ALIPHATIC <adegokefuad1@gmail.com>
2023-11-08 22:23:48 +0100

    Refactoring

--- summarize.js
+++ summarize.js
@@ -1,41 +1,38 @@
 // Axios is the library used to make the API call
 const axios = require('axios');
 
+// Define the Hugging Face API endpoint and access token
+const HF_API_ENDPOINT = 'https://api-inference.huggingface.co/models/facebook/bart-large-cnn';
+const HF_ACCESS_TOKEN = process.env['ACCESS_TOKEN'];
 
-
-// This is the function where the call to the API is made. Returns the summarized text as a string.
-
+// This is the function where the call to the Hugging Face API is made. Returns the summarized text as a string.
 async function summarize(text) {
+  try {
+    const requestBody = {
+      inputs: text,
+      parameters: {
+        max_length: 100,
+        min_length: 30,
+      },
+    };
 
-  // INSERTING CODE SNIPPET FROM POSTMAN HERE
-let data = JSON.stringify({
-  "inputs": text,
-  "parameters": {
-    "max_length": 100,
-    "min_length": 30
-  }
-});
+    const response = await axios.post(HF_API_ENDPOINT, requestBody, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${HF_ACCESS_TOKEN}`,
+      },
+    });
 
-let config = {
-  method: 'post',
-  maxBodyLength: Infinity,
-  url: 'https://api-inference.huggingface.co/models/facebook/bart-large-cnn',
-  headers: { 
-    'Content-Type': 'application/json', 
-    'Authorization': 'Bearer ' + process.env['ACCESS_TOKEN']
-  },
-  data : data
-};
-
-try {
-    const response = await axios.request(config);
-    return response.data[0].summary_text;
-}
-catch (error) {
-    console.log(error);
+    if (response.data && response.data[0] && response.data[0].summary_text) {
+      return response.data[0].summary_text;
+    } else {
+      throw new Error('Invalid response from the Hugging Face API.');
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
   }
 }
 
-// Allows for summarizeText() to be called outside of this file
-
-module.exports = summarizeText;
+// Export the summarize function to make it available for use in other modules
+module.exports = summarize;
